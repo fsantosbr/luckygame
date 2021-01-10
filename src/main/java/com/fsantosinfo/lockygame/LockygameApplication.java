c74fba60e711130305753d9c14df2c73f9a42bbf
Fabio Santos <fsantosinfo@gmail.com>
2021-01-10 13:40:16 -0300

    Project created

---
+++ src/main/java/com/fsantosinfo/lockygame/LockygameApplication.java
@@ -0,0 +1,13 @@
+package com.fsantosinfo.lockygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockygameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockygameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsantosinfo/lockygame/LockygameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsantosinfo.lockygame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockygameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
